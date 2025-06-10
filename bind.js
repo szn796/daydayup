59f0deb87a7ef88931b9647f5c490451a357adb2
szn <474684412@qq.com>
2025-06-10 22:30:00 +0800

    fix: 1

--- bind.js
+++ bind.js
@@ -1,16 +1,17 @@
 Function.prototype.myBind = function (ctx, ...args) {
-    return function A(...args2) {
-        if (Object.getPrototypeOf(A) === this) {
-            return new fn(...[...args, ...args2])
-        } else {
-            return fn.call(ctx, ...[...args]);
-        }
+  return function A (...args2) {
+    if (Object.getPrototypeOf(A) === this) {
+      return new fn(...[...args, ...args2])
+    } else {
+      return fn.call(ctx, ...[...args]);
     }
+  }
 }
-function fn(a, b) {
-    console.log(this, a, b)
+function fn (a, b) {
+  console.log(this, a, b)
 }
 const obj = {
-    test: 'test'
+  test: 'test'
 }
 fn.myBind(obj, 1, 2)()
+11

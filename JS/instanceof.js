95d7c7acd323cea10e72640bd84d0ca27928fc36
Szn <sunzhennan@linhuiba.com>
2025-06-11 21:04:10 +0800

    feat: 0611-原型链-todo

--- JS/instanceof.js
+++ JS/instanceof.js
@@ -3,13 +3,21 @@
  */
 
 function myInstanceOf (obj, constructor) {
-  if (typeof obj !== 'object' && obj === null) return;
+  if (typeof obj !== 'object' || obj === null) return false;
   let proto = Object.getPrototypeOf(obj);
   while (proto) {
     if (proto === constructor.prototype) {
       return true;
     }
-    proto = Object.gerPrototypeOf(proto);
+    proto = Object.getPrototypeOf(proto);
   }
   return false
 }
+
+
+
+console.log(myInstanceOf(1, Number));
+console.log(myInstanceOf(1, String));
+console.log(myInstanceOf([], Array));
+console.log(myInstanceOf([], Object));
+console.log([] instanceof Object);
